4ce10835f5f821c0c55dee9ea0f0eefde54bc1c7
Sybil CK Wong <sybil.wong@hotmail.co.uk>
2014-02-19 16:54:34 +0000

    Seattle bkgd

--- app.rb
+++ app.rb
@@ -6,16 +6,21 @@ get '/' do
 end
 
 get '/todo' do
-  @todos = ['Order canapes', 'Confirm event details with attendees', 'Book band']
+  @todos = [
+    'Order canapes',
+    'Confirm event details with attendees',
+    'Book band', #comma at start makes it easy to comment out item?
+    'Buy drinks'
+  ]
 
   erb :todo
 end
 
 get '/schedule' do
   @schedule = [ ['8pm',    'Doors open'],
-                ['8.30pm', 'Champagne and canapes served'],
-                ['9pm',    'Speech'],
                 ['9.30pm', 'Painting unveiled'],
+                ['9pm',    'Speech'],
+                ['8.30pm', 'Champagne and canapes served'],
                 ['9.40pm', 'Live band plays'] ]
 
   erb :schedule
@@ -26,11 +31,17 @@ get '/rsvps' do
 
   @acceptances = []
   @rejections  = []
-  @acceptance_count = 0
-  @rejection_count  = 0
 
-  # TODO categorise rsvps into acceptances/rejections and count them
+  @rsvps.each do |rsvpPerson|
+    if rsvpPerson[1] == "yes"
+      @acceptances << rsvpPerson[0]
+    else
+      @rejections << rsvpPerson[0]
+    end
+  end
 
+  @acceptance_count = @acceptances.length
+  @rejection_count  = @rejections.length
 
   erb :rsvps
 end
